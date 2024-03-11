a67d35d5d0b4b7ef60ee8699359d02390ab14d04
Service Web <sw@gssmail.com>
2024-03-11 08:50:04 -0500

    Commit changes in C:\Websites\Git\emarlow\3867\ATG_3867_CRM_DocTemp.sql

---
+++ ATG_3867_CRM_DocTemp.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "ATG_3867_CRM_DocTemp"(
+ "TDID" IDENTITY DEFAULT '0',
+ "Template_Name" CHAR(100),
+ "Path" CHAR(150),
+ "Filename" CHAR(50),
+ "Security_Group" CHAR(50),
+ "UserID" INTEGER,
+ "LastModifiedDT" DATETIME);
